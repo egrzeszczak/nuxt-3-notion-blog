67d8ec5cc9160660483ce3f64a07f27ab8fb4006
Ernest Grzeszczak <ernest.grzeszczak@gmail.com>
2022-05-24 21:17:25 +0200

    style: :art: Page padding fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="max-w-6xl mx-auto h-screen flex flex-col">
         <Header />
-        <NuxtPage class="flex-1" />
+        <NuxtPage class="flex-1 p-4 xl:p-0 " />
         <Footer />
     </div>
 </template>
