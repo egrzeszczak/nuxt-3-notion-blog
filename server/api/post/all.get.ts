e651a5f629969d39d02999d942a83c3effefe163
Ernest Grzeszczak <ernest.grzeszczak@gmail.com>
2022-05-27 12:40:19 +0200

    fix: :arrow_down: API sends posts in descending order now

--- server/api/post/all.get.ts
+++ server/api/post/all.get.ts
@@ -18,7 +18,7 @@ export default defineEventHandler((event) => {
         sorts: [
             {
                 property: "Date",
-                direction: "ascending",
+                direction: "descending",
             },
         ],
     });
