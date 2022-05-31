3dd64d06a5564f35aba02e674a2eef645921e60c
Ernest Grzeszczak <ernest.grzeszczak@gmail.com>
2022-05-31 11:44:48 +0200

    style: :art: Small layout height change

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="">
         <Header />
-        <div class="max-w-6xl mx-auto flex flex-col">
+        <div class="min-h-screen max-w-6xl mx-auto flex flex-col">
             <NuxtPage class="flex-1 p-4 xl:p-0" />
         </div>
         <Footer />
