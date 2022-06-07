9b031d084ff83a4967b918679f9b3bc333cd43df
egrzeszczak <ernest.grzeszczak@protonmail.com>
2022-06-07 18:57:39 +0200

    fix: :wastebasket: No cust scrollbar for now

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,5 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    css: ["@/assets/css/tailwind.css"],
     buildModules: ["@nuxtjs/tailwindcss"],
 });
