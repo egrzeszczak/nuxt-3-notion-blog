6ebdb6c0a070c84149154cc93f3cae376df7b04a
Ernest Grzeszczak <ernest.grzeszczak@gmail.com>
2022-06-07 15:07:55 +0200

    feat: :sparkles: Added Toggle heading 1, 2, 3 to Notion components

--- components/Notion/Render.vue
+++ components/Notion/Render.vue
@@ -1,10 +1,29 @@
 <template>
     <div v-for="block in content" :key="block.id">
-        <NotionHeading1 v-if="block.type == 'heading_1' && !block.has_children" :block="block">
+        <NotionHeading1
+            v-if="block.type == 'heading_1' && !block.has_children"
+            :block="block"
+        >
         </NotionHeading1>
-        <NotionHeading2 v-else-if="block.type == 'heading_2' && !block.has_children" :block="block">
+        <NotionToggle
+            v-else-if="
+                (block.type == 'heading_1' ||
+                    block.type == 'heading_2' ||
+                    block.type == 'heading_3') &&
+                block.has_children
+            "
+            :block="block"
+        >
+        </NotionToggle>
+        <NotionHeading2
+            v-else-if="block.type == 'heading_2' && !block.has_children"
+            :block="block"
+        >
         </NotionHeading2>
-        <NotionHeading3 v-else-if="block.type == 'heading_3' && !block.has_children" :block="block">
+        <NotionHeading3
+            v-else-if="block.type == 'heading_3' && !block.has_children"
+            :block="block"
+        >
         </NotionHeading3>
         <NotionParagraph v-else-if="block.type == 'paragraph'" :block="block">
         </NotionParagraph>
@@ -17,9 +36,15 @@
         <NotionToDo v-else-if="block.type == 'to_do'" :block="block" />
         <NotionTable v-else-if="block.type == 'table'" :block="block" />
         <NotionToggle v-else-if="block.type == 'toggle'" :block="block" />
-        <NotionEquation v-else-if="block.type == 'equation'" :expression="block.equation.expression" />
+        <NotionEquation
+            v-else-if="block.type == 'equation'"
+            :expression="block.equation.expression"
+        />
         <NotionDivider v-else-if="block.type == 'divider'" />
-        <NotionColumnList v-else-if="block.type == 'column_list'" :block="block"/>
+        <NotionColumnList
+            v-else-if="block.type == 'column_list'"
+            :block="block"
+        />
         <NotionTableOfContents
             v-else-if="block.type == 'table_of_contents'"
             :content="
@@ -32,7 +57,7 @@
                 })
             "
         />
-        <NotionBookmark v-else-if="block.type == 'bookmark'" :block="block"/>
+        <NotionBookmark v-else-if="block.type == 'bookmark'" :block="block" />
         <!-- TODO: LI grouping -->
         <li
             v-else-if="
@@ -43,12 +68,15 @@
             <NotionRichText :richText="block[block.type].rich_text" />
         </li>
 
-        <div v-else class="text-red-800 bg-red-100 dark:bg-red-900 dark:text-red-100 text-center p-4 rounded text-xs my-2">
-            '{{block.type}}' Block type unsupported
+        <div
+            v-else
+            class="text-red-800 bg-red-100 dark:bg-red-900 dark:text-red-100 text-center p-4 rounded text-xs my-2"
+        >
+            '{{ block.type }}' Block type unsupported
             <pre class="text-left">
-                {{block}}
+                {{ block }}
             </pre>
-        </div>  
+        </div>
     </div>
 </template>
 
--- components/Notion/Toggle.vue
+++ components/Notion/Toggle.vue
@@ -1,7 +1,27 @@
 <template>
-    <div class="collapse collapse-arrow">
+    <div
+        class="collapse collapse-arrow border-base-200 border bg-base-200 rounded-box my-2"
+    >
         <input type="checkbox" />
-        <div class="collapse-title text-md font-medium">
+        <NotionHeading1
+            class="collapse-title"
+            v-if="block.type == 'heading_1'"
+            :block="block"
+        >
+        </NotionHeading1>
+        <NotionHeading2
+            class="collapse-title"
+            v-else-if="block.type == 'heading_2'"
+            :block="block"
+        >
+        </NotionHeading2>
+        <NotionHeading3
+            class="collapse-title"
+            v-else-if="block.type == 'heading_3'"
+            :block="block"
+        >
+        </NotionHeading3>
+        <div v-else class="collapse-title">
             <NotionRichText :richText="block[block.type].rich_text  "/>
         </div>
         <div class="collapse-content">
